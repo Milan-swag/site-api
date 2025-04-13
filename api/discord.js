6905a2b76874b02b3cc7c4afcbe7e8cbcbe94d44
Milan-swag <milan.duettra@gmail.com>
2025-04-13 13:02:38 +0200

    Update discord.js

--- api/discord.js
+++ api/discord.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
 
       const discordPayload = {
-        content: `📧 Glückwunsch Milan! Du hast eine neue Bestellung für ${data.type || "Nicht angegeben"} von deinem Store erhalten!\n——————————\n • 🛒 Typ: ${data.type || "Nicht angegeben"}\n • 📆 Datum: ${data.date}\n • ⏰ Zeit: ${data.time}\n • 👤 Name: ${data.name}\n • 📧 Email: ${data.email}\n • 🏠 Adresse: ${data.street}\n • 📞 Telefon: ${data.phone}`
+        content: `📧 Glückwunsch Milan! Du hast eine neue Bestellung für ${data.type || "Nicht angegeben"} von deinem Store erhalten!\n——————————\n ・ 🛒 Typ: ${data.type || "Nicht angegeben"}\n ・ 📆 Datum: ${data.date}\n ・ ⏰ Zeit: ${data.time}\n ・ 👤 Name: ${data.name}\n ・ 📧 Email: ${data.email}\n ・ 🏠 Adresse: ${data.street}\n ・ 📞 Telefon: ${data.phone}`
       };
 
       const response = await fetch(webhookUrl, {
